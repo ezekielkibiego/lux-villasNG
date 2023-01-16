3426f54c32f5ca0b91a62721e10f2fb3c66bd0af
Ezekiel Kibiego <kibiezekiel@gmail.com>
2023-01-16 23:23:19 +0000

    add sign in with google

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -6,14 +6,16 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-
+import {GoogleLoginProvider, SocialAuthServiceConfig, SocialLoginModule} from 'angularx-social-login';
+import { AppComponent } from '../app.component';
 
 
 @NgModule({
   declarations: [
     AuthComponent,
     SignupComponent,
-    LoginComponent
+    LoginComponent,
+   
   ],
   imports: [
     CommonModule,
@@ -21,6 +23,27 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
     AuthRoutingModule,
     ReactiveFormsModule,
     FormsModule,
-  ]
+    SocialLoginModule
+  ],
+  providers: [
+  
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '535807003840-he7rmt8pr8h715dpr04rr978rkiaqt7g.apps.googleusercontent.com'
+            )
+          }
+        ]
+      } as SocialAuthServiceConfig,
+    }
+  ],
+  bootstrap: [AppComponent]
+
+  
 })
 export class AuthModule { }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,19 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/Services/user.service';
-
+import {GoogleLoginProvider, SocialAuthService, SocialUser} from 'angularx-social-login';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit{
+  user: SocialUser | undefined;
+  loggedIn: boolean | undefined;
+
+
 
   register:any;
+ 
 
-  constructor(private userService: UserService, private router : Router) {}
+  constructor(private userService: UserService, private router : Router, private authService: SocialAuthService) {}
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
   
   ngOnInit(): void {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      console.log(user);
+      this.loggedIn = (user != null);
+    });
      this.register = {
         username: '',
         email: '',
